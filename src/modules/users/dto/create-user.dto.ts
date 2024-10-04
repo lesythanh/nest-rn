af7d455efa8c696d9d5f356440c7afe54f7f8202
Jay <jay@bradshawgroup.global>
2024-10-04 16:59:45 +0700

    create users

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    image: string;
+
+}
